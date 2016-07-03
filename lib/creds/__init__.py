a42c8bb90bad438e8d5a092e5dec46518a468349
Jon Hadfield <jon@lessknown.co.uk>
2016-07-03 16:19:38 +0100

    bump version.

--- lib/creds/__init__.py
+++ lib/creds/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import (unicode_literals, print_function)
 
 __title__ = 'creds'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = 'Jon Hadfield'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Jon Hadfield'
