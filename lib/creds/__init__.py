e38295b432b474efffc04c8056cc642b333d2440
Jon Hadfield <jon@lessknown.co.uk>
2016-08-07 16:35:03 +0100

    pep8.

--- lib/creds/__init__.py
+++ lib/creds/__init__.py
@@ -2,17 +2,17 @@
 """
 This package contains all of the modules utilised by the creds library.
 
-constants: Functions to defines and discover values that remain constant through the management of users and their credentials
+constants: Functions to define and discover OS constants.
 
-plan: Functions to generate a list of steps to transition from the current state to the desired state
+plan: Functions to generate a list of steps to transition from the current state to the desired state.
 
-ssh: Contains a class to represent a users' keys and functions to manage them
+ssh: Contains a class to represent a users' keys and functions to manage them.
 
 users: Functions  to managing users and classes...
  Users - representation of a user list and methods to read, write and manage them
  User - representation of a single user with their associated credentials
 
-utils: Common helper functions
+utils: Common helper functions.
 """
 
 from __future__ import (unicode_literals, print_function)
